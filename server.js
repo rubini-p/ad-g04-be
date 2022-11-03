d0789a13950a3d3358e172ffd9177bd59b6b6d6e
Flor <fviacava@labsxd.com>
2022-11-03 11:18:32 -0300

    server sin comentarios

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//WIP
 const config = require('config');
 const db = config.get('mongoURI');
 
@@ -9,7 +8,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// const recetasRoutes = require('./routes/recetas-routes');
 const usersRoutes = require('./routes/users-routes');
 const menuRoutes = require('./routes/menu-routes');
 const foodRoutes = require('./routes/food-routes');
@@ -19,8 +17,6 @@ const HttpError = require('./models/http-error');
 const app = express();
 
 app.use(bodyParser.json());
-
-// app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -33,7 +29,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use('/api/recetas', recetasRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/menu', menuRoutes);
 app.use('/api/food', foodRoutes);
@@ -43,25 +38,6 @@ app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
     throw error;
 });
-
-// app.use((error, req, res, next) => {
-//     if (req.file) {
-//         fs.unlink(req.file.path, err => {
-//             console.log(err);
-//         });
-//     }
-//     if (res.headerSent) {
-//         return next(error);
-//     }
-//     res.status(error.code || 500);
-//     res.json({ message: error.message || 'An unknown error occurred!' });
-// });
-
-// mongoose
-//     .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => {console.log('connected to db')})
-//     .catch((error) => {console.log(error)
-//     })
 
 mongoose
     .connect(db , {useNewUrlParser: true, useUnifiedTopology: true})
