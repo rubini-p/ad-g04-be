ce9c28ed7846382967283700cc9fa4ec0f4d8713
smithb93 <brendasmith1993@gmail.com>
2022-11-03 11:31:11 -0300

    .

--- routes/calificacion-routes.js
+++ routes/calificacion-routes.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
 
-const calificacionController = require('../controllers/calificacion-controllers');
-const fileUpload = require('../middleware/file-upload');
+const calificacionController = require("../controllers/calificacion-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
-router.post('/altaCalificacion', calificacionController.AltaCalificacion);
-router.get('/obtenerCalificaciones/:restaurant', calificacionController.ObtenerCalificacionesRestaurant);
-
+router.post("/altaCalificacion", calificacionController.AltaCalificacion);
+router.get(
+  "/obtenerCalificaciones/:restaurant",
+  calificacionController.ObtenerCalificacionesRestaurant
+);
 
 module.exports = router;
