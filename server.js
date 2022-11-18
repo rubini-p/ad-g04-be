56d505c2444735652c8cfbbbd988dbc129a2eed6
Pablo Rubini <pablorubini@gmail.com>
2022-11-17 22:06:04 -0300

    ok

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -40,6 +42,19 @@ app.use('/api/restaurants', restaurantsRoutes);
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
     throw error;
+});
+
+app.use((error, req, res, next) => {
+    if (req.file) {
+        fs.unlink(req.file.path, err => {
+            console.log(err);
+        });
+    }
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
