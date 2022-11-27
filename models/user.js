dc2b038aa4783e44c1a36b7b15b16db68050825a
Pablo Rubini <pablorubini@gmail.com>
2022-11-27 17:58:45 -0300

    ok

--- models/user.js
+++ models/user.js
@@ -4,14 +4,18 @@ const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: false },
+  name: { type: String, required: false , default: 'User' },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   favorite:{ type: [], required: false},
   isAdmin: { type: Boolean, required: true, default: true },
   photo: { type: String, required: true },
   defaultImage: { type: Boolean, required: false, default: true }
-});
+},
+  {
+    versionKey: false,
+  }
+);
 
 userSchema.plugin(uniqueValidator);
 
